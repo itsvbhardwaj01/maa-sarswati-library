360ab8e104d78c55e22de9e48abe5842d7eff3d3
vikash <iamvikash0921@gmail.com>
2023-05-16 04:35:29 +0530

    commit

---
+++ htmls/adminpage.js
@@ -0,0 +1,13 @@
+const userinput = document.getElementById("email");
+const userpassword = document.getElementById("pass");
+const submitbtn = document.getElementById("submit");
+const email1 = "itsvbhardwaj9721@gmail.com";
+const pass1 = "library1";
+
+submitbtn.onclick = function evaluateuser(pass, evaluate) {
+  if (userinput.value == email1 && userpassword.value == pass1) {
+    window.open("/adminpage.html", "_self");
+  } else {
+    alert("wrong credentials");
+  }
+};
